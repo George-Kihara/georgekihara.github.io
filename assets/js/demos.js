3171f55b72f70bec9d8d5dc407fcde0dca4d9004
GeorgeKihara <gegohcomp@gmail.com>
2017-09-24 17:35:49 +0300

    updates

--- assets/js/demos.js
+++ assets/js/demos.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var typed2 = new Typed('#typed2', {
     strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
-    typeSpeed: 0,
+    typeSpeed: 10,
     backSpeed: 0,
     fadeOut: true,
     loop: true
