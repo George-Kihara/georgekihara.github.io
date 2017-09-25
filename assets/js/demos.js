e9fcfec31387f5ca851c58af56bd80faea09fe48
GeorgeKihara <gegohcomp@gmail.com>
2017-09-25 14:57:19 +0300

    added typedjs

--- assets/js/demos.js
+++ assets/js/demos.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     typeSpeed: 100,
@@ -6,89 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
     startDelay: 1000,
     loop: true,
     loopCount: Infinity,
-    onComplete: function(self) { prettyLog('onComplete ' + self) },
-    preStringTyped: function(pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
-    onStringTyped: function(pos, self) { prettyLog('onStringTyped ' + pos + ' ' + self) },
-    onLastStringBackspaced: function(self) { prettyLog('onLastStringBackspaced ' + self) },
-    onTypingPaused: function(pos, self) { prettyLog('onTypingPaused ' + pos + ' ' + self) },
-    onTypingResumed: function(pos, self) { prettyLog('onTypingResumed ' + pos + ' ' + self) },
-    onReset: function(self) { prettyLog('onReset ' + self) },
-    onStop: function(pos, self) { prettyLog('onStop ' + pos + ' ' + self) },
-    onStart: function(pos, self) { prettyLog('onStart ' + pos + ' ' + self) },
-    onDestroy: function(self) { prettyLog('onDestroy ' + self) }
+    onComplete: function (self) { prettyLog('onComplete ' + self) },
+    preStringTyped: function (pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
+    onStringTyped: function (pos, self) { prettyLog('onStringTyped ' + pos + ' ' + self) },
+    onLastStringBackspaced: function (self) { prettyLog('onLastStringBackspaced ' + self) },
+    onTypingPaused: function (pos, self) { prettyLog('onTypingPaused ' + pos + ' ' + self) },
+    onTypingResumed: function (pos, self) { prettyLog('onTypingResumed ' + pos + ' ' + self) },
+    onReset: function (self) { prettyLog('onReset ' + self) },
+    onStop: function (pos, self) { prettyLog('onStop ' + pos + ' ' + self) },
+    onStart: function (pos, self) { prettyLog('onStart ' + pos + ' ' + self) },
+    onDestroy: function (self) { prettyLog('onDestroy ' + self) }
   });
 
-  document.querySelector('.toggle').addEventListener('click', function() {
-    typed.toggle();
-  });
-  document.querySelector('.stop').addEventListener('click', function() {
-    typed.stop();
-  });
-  document.querySelector('.start').addEventListener('click', function() {
-    typed.start();
-  });
-  document.querySelector('.reset').addEventListener('click', function() {
-    typed.reset();
-  });
-  document.querySelector('.destroy').addEventListener('click', function() {
-    typed.destroy();
-  });
-  document.querySelector('.loop').addEventListener('click', function() {
-    toggleLoop(typed);
-  });
 
   var typed2 = new Typed('#typed2', {
-    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
-    typeSpeed: 10,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: true
-  });
-  document.querySelector('.loop2').addEventListener('click', function() {
-    toggleLoop(typed2);
-  });
-
-  new Typed('#typed3', {
-    strings: ['My strings are: <i>strings</i> with', 'My strings are: <strong>HTML</strong>', 'My strings are: Chars &times; &copy;'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    smartBackspace: true,
-    loop: true
-  });
-
-  new Typed('#typed4', {
-    strings: ['Some strings without', 'Some HTML', 'Chars'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    attr: 'placeholder',
-    bindInputFocusEvents: true,
-    loop: true
-  });
-
-  new Typed('#typed5', {
-    strings: ['if you wish to contact me', '2 Some <strong>HTML</strong>', '3 Chars &times; &copy;'],
-    typeSpeed: 300,
-    backSpeed: 400,
-    shuffle: true,
-    startDelay: 1000,
-    cursorChar: '_',
-    smartBackspace: true,
-    loop: true,
-    loopCount: Infinity
-  });
-
-  new Typed('#typed6', {
-    strings: ['npm install^1000\n`installing components...` ^1000\n`Fetching from source...`'],
-    typeSpeed: 40,
+    strings: ['Mobile No: +254 713220636 <br> Email: gegohcomp@gmail.com'],
+    typeSpeed: 100,
     backSpeed: 100,
-    startDelay: 1000,
-    loop: true
+    fadeOut: false,
+    loop: 1
   });
-});
+
+  
 
 function prettyLog(str) {
   console.log('%c ' + str, 'color: green; font-weight: bold;');
-}
+};
 
 function toggleLoop(typed) {
   if (typed.loop) {
@@ -96,4 +39,4 @@ function toggleLoop(typed) {
   } else {
     typed.loop = true;
   }
-}
+}});
